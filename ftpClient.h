74a4dae8695af19a67596d8beebf14003a5cbe30
Martim <mitram790@gmail.com>
2022-12-16 14:58:17 +0000

    Correctly Ending connection Macro to print the communication

--- ftpClient.h
+++ ftpClient.h
@@ -2,8 +2,21 @@
 #define __FTPCLIENT__
 
 #include "ftpPath.h"
+/*
+ * Given a ftpPath and an options object
+ * Initializes the ftpConnection in passive mode and returns the file
+ * descriptor of the socket to read from.
+*/
 
-int ftpInit(FtpPath *ftpPath);
+enum FtpAction {
+    FtpDownload,
+    FtpList,
+};
+int ftpInit(FtpPath *ftpPath,enum FtpAction action);
+
+/*
+ * Ends the Connection 
+*/
 int ftpQuit();
 
 #endif // !__FTPCLIENT__
